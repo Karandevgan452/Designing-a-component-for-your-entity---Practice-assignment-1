df9352272cf467a76351b10fed0b3a66c6608e3b
name <karankarandevgan@gmail.com>
2025-03-11 12:37:04 +0530

    Assignment completed

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,32 @@
-// write the component code here
+const UserCard = () => {
+  // Dummy static data
+  const profilePhoto = "https://www.w3schools.com/w3images/avatar3.png";
+  const name = "Karan";
+  const email = "karan@gmail.com";
+  const phoneNumber = "1234567890";
+  const address = "1865, Basant Vihar, Kalka";
+
+  return (
+    <div className="card-container">
+      <div className="user-card">
+        <div className="profile-pic">
+          <img src={profilePhoto} alt="Profile" />
+        </div>
+        <div className="user-details">
+          <h1>{name}</h1>
+          <h2>
+            <strong>Email:</strong> {email}
+          </h2>
+          <h2>
+            <strong>Phone:</strong> {phoneNumber}
+          </h2>
+          <h2>
+            <strong>Address:</strong> {address}
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
